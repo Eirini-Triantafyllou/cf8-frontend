05d7a49da1eb92d8aea949b9b6d6c3ada41a8831
E. Triantafyllou <irenetriantafyllou@msn.com>
2025-11-07 18:19:07 +0200

    Template Form

--- src/app/components/step8-template-driven-forms/person-template-form/person-template-form.ts
+++ src/app/components/step8-template-driven-forms/person-template-form/person-template-form.ts
@@ -1,16 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output, ViewChild } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule, NgForm } from '@angular/forms';
 import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatButtonModule } from '@angular/material/button';
 import { Person } from '../../../shared/interfaces/person';
-import { MatFormFieldModule } from '@angular/material/form-field';
 
 @Component({
   selector: 'app-person-template-form',
   imports: [
     CommonModule,
-    FormsModule, 
+    FormsModule,
     MatFormFieldModule,
     MatInputModule,
     MatButtonModule
@@ -19,8 +19,19 @@ import { MatFormFieldModule } from '@angular/material/form-field';
   styleUrl: './person-template-form.css',
 })
 export class PersonTemplateForm {
+  @Output() personOutput = new EventEmitter<Person>();
+  @ViewChild('eForm', {static:false}) form:NgForm | undefined;
+
   onSubmit(value:any){
     console.log(value);
+    this.personOutput.emit(value);
   }
 
+  OnSetValue(){
+    this.form?.setValue({
+      firstname: "lakis",
+      lastname: "lalakis",
+      email:"lakis@aueb.gr"
+    })
+  }
 }
